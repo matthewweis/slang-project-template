c14781544e05ca5ec252d06398139dbd5a298824
Matthew Weis <matthewweis@ksu.edu>
2022-05-29 18:17:07 -0500

    use relative paths but global callable

--- BUILD/checksum_create.sh
+++ BUILD/checksum_create.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-shasum -a 512 $DIR/SHELLSHOCK > $DIR/SHA512SUM
+shasum -a 512 "$( basename "$DIR/SHELLSHOCK" )" > "$( basename "$DIR/SHA512SUM" )"
 chmod a+r $DIR/SHA512SUM
 chmod a-w $DIR/SHA512SUM
 chmod a-x $DIR/SHA512SUM
